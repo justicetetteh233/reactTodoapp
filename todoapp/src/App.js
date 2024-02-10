d986b19a678013691d56aaf4ddb6fd94bf42adac
justice <justice@DESKTOP-3NGKVBS.localdomain>
2024-02-10 04:03:23 +0000

    understanding react

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -5,6 +5,40 @@ function App() {
   return (
     <div className="App">
      <h1>My Todos</h1>
+
+     <div className = 'todo-wrapper'>
+        <div className= 'todo-input'>
+
+          <div className='todo-input-item'>
+            <label>Title</label>
+            <input type="text" placeholder="what is the title?" />
+          </div>
+
+          <div className='todo-input-item'>
+            <label>Discription</label>
+            <input type="text" placeholder="what is the title?" />
+          </div>
+
+          <div className='todo-input-item'>
+            <button type= 'button'className = 'primaryBtn'>Add</button>
+          </div>
+
+        </div>
+
+        <div className = 'btn-area'>
+          <button>todo</button>
+          <button>completed</button>
+        </div>
+
+        <div className = 'todo-list'>
+          <div className = 'todo-list-item'>
+            <h1>task 1</h1>
+            <h1>some discription</h1>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
